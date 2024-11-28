b32d5c0c30b45bed13f1243fdb4d31637859018f
Kodzito11 <Kos001@edu.zealand.dk>
2024-11-28 13:03:33 +0100

    Jeg har lavet en test til at verificere om data bliver tilsendt fra klient til server

--- CO2StatisticRestApiTests/Controllers/DBContextTests.cs
+++ CO2StatisticRestApiTests/Controllers/DBContextTests.cs
@@ -1,4 +1,5 @@
 using CO2StatisticRestApi;
+using System.Net.Sockets;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace CO2StatisticRestApiTests;
@@ -17,18 +18,40 @@ public class DBContextTests
         // Act
         bool isValid1 = ValidateData(invalidData1);
         bool isValid2 = ValidateData(invalidData2);
-        bool isValid2 = ValidateData(validData);
+		bool isValid2 = ValidateData(validData);
 
         // Assert
         Assert.IsFalse(isValid1, "CO2-level cannot be null.");
-        Assert.IsFalse()
-        Assert.IsTrue(isValid2, "CO2-level is ");
-    }
+		Assert.IsInstanceOfType(invalidData2, typeof(int), "Return value is not an int.");
+		Assert.IsTrue(isValid2, "CO2-level is ");
+
+
+	}
     }
 
     [TestMethod]
+	public void Test_DataSentOverUDP()
+	{
+		// Arrange
+		var udpClient = new Mock<IUdpClient>(); 
+		var sensor = new RaspberryPiSensor(udpClient.Object);
+		var co2Data = 400;
+
+		// Act
+		sensor.SendDataOverUDP(co2Data);
+
+		// Assert
+		udpClient.Verify(client => client.Send(
+			It.IsAny<byte[]>(),
+			It.IsAny<int>(),   
+			It.IsAny<string>(),
+			It.IsAny<int>()
+		), Times.Once, "UDP data was not sent correctly.");
+	}
+
+
+	[TestMethod]
     public void GetDataTest() { }
 
-    [TestMethod]
 
 }
