280aadda1fe27ff284fa86aed16ea110ab1729c9
Jeppe August Hirsch Jeppsson <jeppe.jeppsson@live.dk>
2024-12-03 11:33:52 +0100

    login + create user

--- CO2StatisticRestApi/Services/UserRepository.cs
+++ CO2StatisticRestApi/Services/UserRepository.cs
@@ -1,9 +1,14 @@
 ﻿using CO2StatisticRestApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
 
 namespace CO2StatisticRestApi.Services
 {
     public class UserRepository : DBConnection
     {
+        private const int _saltLength = 16;
+        private const int _storedPasswordLength = 20;
+        private const int _hashIterations = 10000;
         public User? GetById(int id)
         {
             return _dbContext.Users.FirstOrDefault(user => user.Id == id);
@@ -11,12 +16,56 @@ namespace CO2StatisticRestApi.Services
 
         public User? Login(string email, string password)
         {
-            return _dbContext.Users.FirstOrDefault(user => user.Email == email && user.UserPassword == password);
+            User? user = _dbContext.Users.FirstOrDefault(user => user.Email == email);
+            if (user == null)
+                return null;
+            if (!VerifyPassword(password, user))
+                return null;
+
+            return user;
         }
         public User? Create(string email, string password)
         {
-            // remember to hash the password
-            throw new NotImplementedException("Yikes, you have to do something?!");
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+                return null;
+            var user = new User() { Email = email, UserPassword = HashPassword(password) };
+            _dbContext.Users.Add(user);
+            _dbContext.SaveChanges();
+            return user;
+        }
+        private string HashPassword(string password)
+        {
+            // random salt to make it harder to brute force
+            byte[] salt = RandomNumberGenerator.GetBytes(_saltLength);
+            // the hashing algorithm 
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, _hashIterations);
+            byte[] hash = pbkdf2.GetBytes(_storedPasswordLength);
+
+            // store the salt, then the hashed password
+            byte[] hashBytes = new byte[_saltLength + _storedPasswordLength];
+            Array.Copy(salt, 0, hashBytes, 0, _saltLength);
+            Array.Copy(hash, 0, hashBytes, _saltLength, _storedPasswordLength);
+            // convert from byte-array to string
+            string hashedPassword = Convert.ToBase64String(hashBytes);
+            return hashedPassword;
+        }
+        private bool VerifyPassword(string password, User user)
+        {
+            // the stored salt + password
+            string savedPasswordHash = user.UserPassword;
+            // convert to byte-array
+            byte[] hashBytes = Convert.FromBase64String(savedPasswordHash);
+            // get the salt
+            byte[] salt = new byte[_saltLength];
+            Array.Copy(hashBytes, 0, salt, 0, _saltLength);
+            // run the hashing algorithm
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, _hashIterations);
+            byte[] hash = pbkdf2.GetBytes(_storedPasswordLength);
+            // see if we got the same value
+            for (int i = 0; i < _storedPasswordLength; i++)
+                if (hashBytes[i + _saltLength] != hash[i])
+                    return false;
+            return true;
         }
 
         public bool ChangeEmail(string oldEmail, string password, string newEmail)
