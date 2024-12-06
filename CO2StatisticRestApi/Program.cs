f6100e0dca7b0e0b7c0f9fdc042e2a6b1a7d5094
Jeppe Jeppsson <jeppe.jeppsson@live.dk>
2024-12-06 14:34:53 +0100

    Updated databaselib

--- CO2StatisticRestApi/Program.cs
+++ CO2StatisticRestApi/Program.cs
@@ -2,6 +2,7 @@ using CO2StatisticRestApi;
 using Microsoft.EntityFrameworkCore;
 using CO2DatabaseLib;
 using CO2DatabaseLib.Models;
+using Microsoft.Extensions.DependencyInjection;
 
 //var optionsBuilder = new DbContextOptionsBuilder<DbContextCO2>();
 //optionsBuilder.UseSqlServer("Data Source=mssql17.unoeuro.com;Initial Catalog=jeppejeppsson_dk_db_test;Persist Security Info=True;User ID=jeppejeppsson_dk;Password=gk3BR45pbxtGwHnard6f;TrustServerCertificate=True");
@@ -14,7 +15,26 @@ using CO2DatabaseLib.Models;
 
 //return;
 
-using Microsoft.Extensions.DependencyInjection;
+//var startDate = new DateTime(2024, 8, 1);
+//Random rand = new Random();
+//int prevValue = 400;
+//int prevDay = 1;
+//MeasurementRepository repository = new MeasurementRepository();
+//while (startDate < DateTime.Now)
+//{
+//    prevValue += rand.Next(100) - 50;
+//    if (prevValue < 0) prevValue = 0;
+//    if (prevValue > 2000) prevValue = 2000;
+//    repository._dbContext.Measurements.Add(new Measurement() { MeasurementTime = startDate, MeasurementValue = prevValue, SensorId = 1 });
+//    startDate = startDate.AddMinutes(5);
+//    if (prevDay != startDate.Day)
+//    {
+//        prevDay = startDate.Day;
+//        Console.WriteLine(startDate.Day);
+//        repository._dbContext.SaveChanges();
+//    }
+//}
+//return;
 
 DBConnection dBConnection = new DBConnection();
 
